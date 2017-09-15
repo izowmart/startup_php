c9444923173d31a9ffe9830bbe30b54c66a2a196
izowmart <izowmart7@gmail.com>
2017-09-15 13:37:54 +0300

    updated file on the CRUD operations

--- voters_management_system/voters_mng.php
+++ voters_management_system/voters_mng.php
@@ -38,47 +38,41 @@
 	<div class="container-fluid bg-success" >
 		<h3 class="text-center text-info">Post DATA to the DB</h3>
 		<form method="POST" action="voters_mng.php">
-			<div class="row">
+			<div class="row text-center">
 				<div class="col-md-6">
 					<div class="form-group">
-				<label>Name:
-					<input type="text" name="name" class="form-control" placeholder="Enter Your Full Names" required>
-				</label>
-			</div>
-			<div class="form-group">
-				<label>ID:
-					<input type="number" name="ID" class="form-control" placeholder="Enter Your ID" required>
-				</label>
-			</div>
-			<div class="form-group">
-				<label>County:
-					<input type="text" name="County" class="form-control" placeholder="Enter Your County" required>
-				</label>
-			</div>
-					
+						<label>Name:
+							<input type="text" name="Name" class="form-control" placeholder="Enter Your Full Names" required>
+						</label>
+					</div>
+					<div class="form-group">
+						<label>ID:
+							<input type="number" name="ID" class="form-control" placeholder="Enter Your ID" required>
+						</label>
+					</div>
+					<div class="form-group">
+						<label>County:
+							<input type="text" name="County" class="form-control" placeholder="Enter Your County" required>
+						</label>
+					</div>	
 				</div>
 				<div class="col-md-6">
 					<div class="form-group">
-				<label>Date:
-					<input type="date" name="Date" class="form-control" placeholder="Enter The Date" required>
-				</label>
-			</div>
-			<div class="form-group">
-				<label>Polling_station:
-					<input type="text" name="Polling_station" class="form-control" placeholder="Enter Your Polling Station" required>
-				</label>
+						<label>Date:
+							<input type="date" name="Date" class="form-control" placeholder="Enter The Date" required>
+						</label>
+					</div>
+					<div class="form-group">
+						<label>Polling_station:
+							<input type="text" name="Polling_station" class="form-control" placeholder="Enter Your Polling Station" required>
+						</label>
 
-			</div>
-			<div class="form-group">
-				<input type="submit" name="submit" value="Submit" class="btn btn-success">
-			</div>
-					
-				</div>
-				
-			</div>
-			
-			
-			
+					</div>
+					<div class="form-group">
+						<input type="submit" name="submit" value="Submit" class="btn btn-success">
+					</div>	
+				</div>	
+			</div>		
 		</form>
 	</div>
 	<div class="container-fluid">
@@ -92,6 +86,8 @@
 					<th>County</th>
 					<th>Date</th>
 					<th>Poling_station</th>
+					<th id="edit" >Update</th>
+					<th id="del">Delete</th>
 				</tr>
 			</thead>
 			<tbody>
@@ -106,6 +102,8 @@
 							<td>$row[County]</td>
 							<td>$row[Date]</td>
 							<td>$row[Polling_station]</td>
+							<td> <a href ='voters_mng.php?edit_id=$row[No]' class ='btn btn-warning'>Edit</td>
+							<td> <a href ='voters_mng.php?del_id=$row[No]' class ='btn btn-danger'>Delete</td>
 						</tr>";
 				}
 				 ?>
@@ -115,3 +113,48 @@
 	</div>
 </body>
 </html>
+<?php
+ //process user input and push data to the DB
+ 	if (isset($_POST['submit'])) {
+ 	 	$Name = $_POST['Name'];
+ 	 	$ID = $_POST['ID'];
+ 	 	$County = $_POST['County'];
+ 	 	$Date = $_POST['Date'];
+ 	 	$Polling_station = $_POST['Polling_station'];
+// push data to the DB
+ 	 	$insert = "INSERT INTO voters_mngt (No, Name,ID,County,Date,Polling_station) VALUES (Null,'$Name','$ID','$County','$Date','$Polling_station')";
+ 	 	// RUN THE QUERY
+ 	 	$query=mysqli_query($conn,$insert);
+ 	 	if ($query) {
+ 	 		echo "Insert Successful";
+ 	 		?>
+ 	<script>window.location ="voters_mng.php"</script>
+ 	<?php
+	 	} else {
+	 	 	 		// header("location: voters_mng.php");
+		 	die("query failed! <br />" . mysqli_error($conn));
+		  
+		 echo "Query successful <br />";
+	 		
+ 		} 
+
+ 	}
+ 	 
+ ?>
+ <?php 
+ 	if (isset($_GET['del_id'])) {
+ 		$query_del ="DELETE FROM voters_mngt WHERE No ='$_GET[del_id]'";
+ 		// run query
+ 		if (mysqli_query($conn,$query_del)) {
+ 			// reload page if query succeful
+ 			echo "Delete Successful";?>
+ 			<script>
+ 				window.location = "voters_mng.php";
+ 			</script>
+ 			<?php
+ 		}else{
+ 			die("Delete failed: ". mysqli_error($conn));
+ 		}
+ 	}
+  ?>
+ 
